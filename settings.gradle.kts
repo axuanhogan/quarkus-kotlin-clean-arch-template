cc1916997bc385b5adb5a0841faf6b66be34532f
axuan <a80259@gmail.com>
2026-05-10 22:53:07 +0800

    chore: update project name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "clean-architecture-implementation"
+rootProject.name = "quarkus-kotlin-clean-arch-template"
 include("core:domain", "core:application")
 include("common")
 include("web-api")
